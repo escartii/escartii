e0864ae554e0f6b5e10415667ade8cbe8d21eba6
unknown <escartismx@gmail.com>
2022-12-10 22:45:34 +0100

    Ejercicios Powershell

---
+++ PowerShell/Tarea1.2.ps1
@@ -0,0 +1,40 @@
+﻿ 
+ #Alvaro Escarti :))
+
+ #Buenas Angel, he hecho los 3 ejercicios en un mismo script
+
+ $Parameter1 = [String]$args[0]
+
+ #Utilizo la variable args.Count para contar los argumentos que introduces
+Write-Host "Has introducido : "$args.Count "argumentos" 
+
+
+#Si hay un argumento entramos dentro del if, en el siguiente if comprobamos que NO empieza por C:\,lo que quiere decir que es ruta relativa
+if ( $args.Count -eq 1 ){
+
+    if (  $Parameter1.StartsWith(".\") -or $Parameter1.StartsWith("..\" )) {
+        Write-Host "La ruta: [ ** $Parameter1 ** ] Es relativa"
+        #IMPORTANTE !!
+        #Comprobar si existe un directorio o fichero
+        #Tienes que saber en que ruta te encuentras (pwd) para que encuentre el directorio o fichero que quieres
+        if ( Test-Path -Path $Parameter1 ){
+            Write-Host "El fichero o directorio existe..."
+        }else{
+            Write-Host "No he encontrado nada..."
+        }
+        
+    }else{
+        Write-Host "La ruta:"$Parameter1 "Es absoluta"
+    }
+
+}
+
+
+if ( $args.Count -eq 0 -or $args.Count -ge 2 ){
+
+    Write-Host "Introduce un parametro valido"
+ }
+
+
+ #Sacar los permisos de un directorio o fichero 
+ (Get-Acl -Path $Parameter1).Access | Format-Table IdentityReference,FileSystemRights,AccessControlType,IsInherited,InheritanceFlags -AutoSize
---
+++ PowerShell/Tarea1.ps1
@@ -0,0 +1,46 @@
+﻿ 
+ #Alvaro Escarti :))
+
+ #Gracias por la ayudita
+ #Team Pico y Pala 
+
+ $Parameter1 = [String]$args[0]
+
+
+ #Utilizo la variable args.Count para contar los argumentos que introduces
+Write-Host "Has introducido : "$args.Count "argumentos" 
+
+#Si hay un argumento, entramos dentro del if y en el siguiente if comprobamos que empieza por C:\,lo que quiere decir que es ruta absoluta
+if ( $args.Count -eq 1 ){
+
+
+    if ( $Parameter1.StartsWith("C:\") ) {
+
+        Write-Host "La ruta: [ ** "$Parameter1 ** ] "Es absoluta"
+        if ( Test-Path -Path $Parameter1 ){
+            Write-Host "El fichero o directorio existe..."
+        }else{
+            Write-Host "No he encontrado nada..."
+        }
+    }else{
+        Write-Host "La ruta:"$Parameter1 "NO Es absoluta"
+
+    }
+
+
+}
+
+
+if ( $args.Count -eq 0 -or $args.Count -ge 2 ){
+
+    Write-Host "Introduce un parametro valido"
+ }
+
+ #Sacar los permisos de un directorio o fichero 
+ if ((Get-Acl -Path "$Parameter1").Access | Format-Table IdentityReference,FileSystemRights,AccessControlType,IsInherited,InheritanceFlags -AutoSize){
+ 
+ Write-Host "Has introducido:"$Parameter1 "y sus permisos son: "
+ #Vuelvo a poner el comando para que salga la informacion (ya que al hacerlo en un if no lo muestra, solo mi "echo")
+ (Get-Acl -Path "$Parameter1").Access | Format-Table IdentityReference,FileSystemRights,AccessControlType,IsInherited,InheritanceFlags -AutoSize
+ 
+ }
