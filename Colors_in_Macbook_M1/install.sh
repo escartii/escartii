73969d97b3d067ee563074a7a06174ac22386bf9
Alvaro Escarti <escartismx@gmail.com>
2022-12-19 23:33:07 +0100

    Como cambiar los colores del editor de texto nano en MAC M1

--- Colors_in_Macbook_M1/install.sh
+++ Colors_in_Macbook_M1/install.sh
@@ -2,10 +2,10 @@
 
 #Alvaro Escarti :))
 
-#Poner colores en la terminal de MacOS [ Chip M1 ]
+#Poner colores en el editor de texto <nano> de MacOS [ Chip M1 ]
 #Antes que nada, instalar homebrew para instalar el editor nano
 brew install nano
 #Despues, actualizamos nuestro fichero nanorc
 echo 'include "/opt/homebrew/Cellar/nano/*/share/nano/*.nanorc"' >> ~/.nanorc
 
-#Restart ur terminal
+#Reiniciamos la terminal y ya tenemos los colores :)
