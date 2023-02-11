35d9824137a05bb93eaa20d13dade95b8fdb49e7
Álvaro <113770514+escartii@users.noreply.github.com>
2023-02-11 01:22:59 +0100

    Delete Learning_Python directory

--- Learning_Python/Condicionales.py
+++
@@ -1,6 +0,0 @@
-#Alvaro Escarti :))
-
-numero = int(input("Dime un numero: "))
-
-if numero>0:
-    print("El numnero es mayor que 0")
--- Learning_Python/procesador.py
+++
@@ -1,7 +0,0 @@
-import platform
-
-# Obtiene información sobre el procesador
-processor = platform.processor()
-
-# Muestra la información del procesador
-print(processor)
